5254b247595b396b7002a1152a97e6125fc9bb1e
Json Bourne <mysocialmediatwitter@protonmail.com>
2019-10-21 21:32:42 +0530

    knockout fundamentals

---
+++ Chapter2/scripts/person.js
@@ -0,0 +1,24 @@
+
+var person = {
+    firstName: "John",
+    age: 30,
+    about: "John's favorite site is <a href='http://www.packtpub.com'>PacktPub</a>.",
+    favoriteColor: "red",
+    favoriteUrl: "http://www.packtpub.com",
+    favoriteSite: "PackPub"
+};
+
+// viewmodel
+
+var personViewModel = function () {
+    var self = this;
+    self.firstName = ko.observable(person.firstName);
+    self.age = ko.observable(person.age);
+    self.about = ko.observable(person.about);
+    self.favoriteColor = ko.observable(person.favoriteColor);
+    self.favoriteUrl = ko.observable(person.favoriteUrl);
+    self.favoriteSite = ko.observable(person.favoriteSite);
+};
+// apply
+
+ko.applyBindings(personViewModel);
