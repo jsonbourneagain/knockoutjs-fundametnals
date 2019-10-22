7970790064ffe6c966804bb208c35d2b8253cd63
Json Bourne <mysocialmediatwitter@protonmail.com>
2019-10-22 19:53:49 +0530

    added code for with binding in two different ways

--- Chapter2/scripts/person.js
+++ Chapter2/scripts/person.js
@@ -1,19 +1,15 @@
 // model
 var person = {
-    children: [
-        { firstName: "Jonnie", age: 3 },
-        { firstName: "Jane", age: 5 },
-        { firstName: "Richard", age: 7 },
-        { firstName: "Mary", age: 9 },
-    ],
-    showAbout: false
+    wife:{
+        firstName: "Jessica",
+        lastName: "Doe"
+    }
 };
 // viewmodel
 
 var PersonViewModel = function () {
     var self = this;
-    self.children = person.children;
-    self.showAbout = ko.observable(person.showAbout);
+    self.wife = ko.observable(person.wife)
 };
 // apply
 
