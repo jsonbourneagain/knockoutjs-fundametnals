2c1c51080e6f32bf4c74b8e0e9c84b03042af0b5
Json Bourne <mysocialmediatwitter@protonmail.com>
2019-10-22 19:22:13 +0530

    added code for selectedOptions binding for a dropdown

--- Chapter2/scripts/person.js
+++ Chapter2/scripts/person.js
@@ -1,44 +1,14 @@
-
-// var person = {
-//     firstName: "John",
-//     age: 30,
-//     about: "John's favorite site is <a href='http://www.packtpub.com'>PacktPub</a>.",
-//     favoriteColor: "red",
-//     favoriteUrl: "http://www.packtpub.com",
-//     favoriteSite: "PackPub",
-//     isMarried: false,
-//     wife: ""
-// };
+// model
 var person = {
-    children: ["Jonnie", "Jane", "Richard", "Mary"]
+    children: ["Jonnie", "Jane", "Richard", "Mary"],
+    selectedChildren: ["Jonnie", "Richard"]
 };
 // viewmodel
 
-// var personViewModel = function () {
-//     var self = this;
-//     self.firstName = ko.observable(person.firstName);
-//     self.age = ko.observable(person.age);
-//     self.about = ko.observable(person.about);
-//     self.favoriteColor = ko.observable(person.favoriteColor);
-//     self.favoriteUrl = ko.observable(person.favoriteUrl);
-//     self.favoriteSite = ko.observable(person.favoriteSite);
-//     self.growOld = function () {
-//         var previousAge = self.age();
-//         self.age(previousAge + 1);
-//     }
-//     self.aboutEnabled = ko.observable(false);
-//     self.showAbout = function () {
-//         self.aboutEnabled(true);
-//     };
-//     self.hideAbout = function () {
-//         self.aboutEnabled(false);
-//     };
-//     self.isMarried = ko.observable(person.isMarried);
-//     self.wife = ko.observable(person.wife);
-// };
 var PersonViewModel = function(){
     var self = this;
     self.children = person.children;
+    self.selectedChildren = person.selectedChildren
 };
 // apply
 
