43948df53f5a4c836d99a17d9e0654a275773cd3
Json Bourne <mysocialmediatwitter@protonmail.com>
2019-10-23 01:04:38 +0530

    Added a new mvc app with knockout

--- Chapter3/HomeLibrary/HomeLibraryWithKnockout/App_Start/RouteConfig.cs
+++ Chapter3/HomeLibrary/HomeLibraryWithKnockout/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace HomeLibraryWithKnockout
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Library", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
