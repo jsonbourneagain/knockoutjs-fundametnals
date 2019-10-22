7b20adcd1239ce29c47b909900cee25afcff6215
Json Bourne <mysocialmediatwitter@protonmail.com>
2019-10-23 00:04:05 +0530

    Added a new application which uses knockout

--- Chapter3/HomeLibrary/HomeLibrary/App_Start/RouteConfig.cs
+++ Chapter3/HomeLibrary/HomeLibrary/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace HomeLibrary
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Library", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
---
+++ Chapter3/HomeLibrary/HomeLibrary/Controllers/LibraryController.cs
@@ -0,0 +1,46 @@
+﻿using HomeLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HomeLibrary.Controllers
+{
+    public class LibraryController : Controller
+    {
+        // GET: Library
+        private static readonly LibraryModel Library = new LibraryModel();
+        public ActionResult Index()
+        {
+            return View(Library);
+        }
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            return View(Library.GetBook(id));
+        }
+        [HttpPost]
+        public ActionResult Edit(BookModel book)
+        {
+            Library.UpdateBook(book);
+            return RedirectToAction("Index");
+        }
+        public ActionResult Add()
+        {
+            var book = new BookModel
+            {
+                Title = "New Book",
+                Author = "Unknown",
+                Year = DateTime.Now.Year
+            };
+            Library.AddBook(book);
+            return RedirectToAction("Index");
+        }
+        public ActionResult Remove(int id)
+        {
+            Library.RemoveBook(id);
+            return RedirectToAction("Index");
+        }
+    }
+}
