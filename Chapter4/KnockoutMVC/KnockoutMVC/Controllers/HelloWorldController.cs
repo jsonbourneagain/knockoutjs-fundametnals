a1ce5094fa69ff85b7168bf59d729aa74f82fd50
Json Bourne <mysocialmediatwitter@protonmail.com>
2019-10-23 14:51:55 +0530

    developed an application based on Knockout MVC

---
+++ Chapter4/KnockoutMVC/KnockoutMVC/Controllers/HelloWorldController.cs
@@ -0,0 +1,25 @@
+﻿using PerpetuumSoft.Knockout;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using KnockoutMVC.Models;
+
+namespace KnockoutMVC.Controllers
+{
+    public class HelloWorldController : KnockoutController
+    {
+        // GET: HelloWorld
+        public ActionResult Index()
+        {
+            ViewBag.Title = "Hello world";
+            return View(
+                new HelloWorld
+                {
+                    FirstName = "Lavakesh",
+                    LastName = "Pandey"
+                });
+        }
+    }
+}
---
+++ Chapter4/KnockoutMVC/KnockoutMVC/Models/HelloWorld.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Script.Serialization;
+using DelegateDecompiler;
+using Newtonsoft.Json;
+
+namespace KnockoutMVC.Models
+{
+    public class HelloWorld
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        [Computed]
+        public string FullName
+        {
+            get { return FirstName + " " + LastName; }
+        }
+    }
+}
