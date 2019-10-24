daddc17f8ffbe24191110ce278f1b9ca6f133db1
Json Bourne <mysocialmediatwitter@protonmail.com>
2019-10-24 11:06:38 +0530

    added knockout data mapper to the source but didn't work

--- Chapter5/scripts/chapter5.js
+++ Chapter5/scripts/chapter5.js
@@ -1,23 +1,14 @@
 // model
+var libraryModel = { LibraryName: "My home library", AmountOfBooks: "5" };
 var libraryViewModel = {
     LibraryName: ko.observable("My home library"),
-    AmountOfBooks: ko.observable("5"),
-};
-libraryViewModel.HasBooks = ko.computed(function(){
-    return this.AmountOfBooks() > 0
-}, libraryViewModel)
-// var jsonObject = ko.toJS(libraryViewModel);
-// jsonObject = {
-//     LibraryName: "My home library",
-//     AmountOfBooks: "5",
-//     HasBooks: true
-// }
-var jsonString = ko.toJSON(libraryViewModel);
-// jsonString = "{'LibraryName': 'My home library', 'AmountOfBooks': '5', 'HasBooks':true}"
-var jsonObject = JSON.parse(jsonString);
-libraryViewModel.LibraryName(jsonObject.LibraryName);
-libraryViewModel.AmountOfBooks(jsonObject.AmountOfBooks);
+    AmountOfBooks: ko.observable("5")
+}
+// var libraryViewModel = ko.mapping.fromJS(libraryModel);
 
+// getModelFromServer(){
+//     return { LibraryName: "My home library", AmountOfBooks: "5" }
+// }
 // apply
 
 ko.applyBindings(libraryViewModel);
